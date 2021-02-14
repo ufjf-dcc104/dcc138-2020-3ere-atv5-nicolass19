8de4fcedff4f39d25c13902974560d6418c774a7
Nicolas Moraes <nicolas.moraes@estudante.ufjf.br>
2021-02-14 01:29:26 -0300

    Adiciona passoa a Cena e Sprite.

--- js/Cena.js
+++ js/Cena.js
@@ -18,6 +18,11 @@ export default class Cena{
     adicionar (sprite){
         this.sprites.push(sprite);
     }
+    passo(dt){
+        for (const sprite of this.sprites) {
+            sprite.passo(dt);
+        }
+    }
 }
 
 
