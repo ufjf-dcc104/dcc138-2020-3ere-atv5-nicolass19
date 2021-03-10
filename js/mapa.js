939088e69229e74666b84cbba4df7a3d0516ccc4
Nicolas Moraes <nicolas.moraes@estudante.ufjf.br>
2021-03-09 22:34:16 -0300

    Simplifica desenho do mapa

--- js/mapa.js
+++ js/mapa.js
@@ -18,25 +18,23 @@ export default class Mapa{
                 switch (this.tiles[l][c]){
                     case 1:
                         ctx.fillStyle = "SlateGray";
-                        ctx.fillRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE);
                         ctx.lineWidth = 1;
                         ctx.strokeStyle = "white";
-                        ctx.strokeRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE);
+                        
                     break;
                     case 2:
                         ctx.fillStyle = "black";
-                        ctx.fillRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE);
                         ctx.lineWidth = 1;
                         ctx.strokeStyle = "white";
-                        ctx.strokeRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE);
+                       
                     break;
                     default:
-                        ctx.fillStyle = "yellow";
-                        ctx.fillRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE);
+                        ctx.fillStyle = "yellow";                       
                         ctx.lineWidth = 1;
-                        ctx.strokeStyle = "yellow";
-                        ctx.strokeRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE);
+                        ctx.strokeStyle = "yellow";        
                 }
+                ctx.fillRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE);
+                ctx.strokeRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE);
             }
         }
     }
