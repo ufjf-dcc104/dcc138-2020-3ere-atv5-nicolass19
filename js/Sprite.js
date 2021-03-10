78a13c8cdacd5571dfb56af90becf5e16662854c
Nicolas Moraes <nicolas.moraes@estudante.ufjf.br>
2021-03-09 23:11:23 -0300

    Adiciona restriçoes a esquerda

--- js/Sprite.js
+++ js/Sprite.js
@@ -1,13 +1,14 @@
 export default class Sprite{
-    /*É responsável por modelar algo que se move na tela.
+    /*
+    É responsável por modelar algo que se move na tela.
     */
-    constructor({x=100, y=100, w=20, h=20, color ="white", vx=0, vy=0}={}){
-        this.x=x;
-        this.y=y;
-        this.vx=vx;
-        this.vy=vy;
-        this.w=w;
-        this.h=h;
+    constructor({x=100, y=100, w=20, h=20, color= "white", vx = 0, vy=0}={}){
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.w = w;
+        this.h = h;
         this.color = color;
         this.cena = null;
         this.mx = 0;
@@ -21,16 +22,15 @@ export default class Sprite{
         this.mx*this.cena.mapa.SIZE,
         this.my*this.cena.mapa.SIZE,
         this.cena.mapa.SIZE,
-        this.cena.mapa.SIZE
-      );
+        this.cena.mapa.SIZE,
+        );
     }
-    passo (dt){
-        this.x = this.x + this.vx*dt; 
+    passo(dt){
+        this.x = this.x + this.vx*dt;
         this.y = this.y + this.vy*dt;
         this.mx = Math.floor(this.x / this.cena.mapa.SIZE);
         this.my = Math.floor(this.y / this.cena.mapa.SIZE);
     }
-
     colidiuCom(outro){
         return !(
             this.x - this.w/2 > outro.x + outro.w/2 ||
@@ -38,8 +38,13 @@ export default class Sprite{
             this.y - this.h/2 > outro.y + outro.h/2 ||
             this.y + this.h/2 < outro.y - outro.h/2
         );
-    }
+}
     aplicaRestricoes(dt) {
+        this.aplicaRestricoesDireita(dt);
+        this.aplicaRestricoesEsquerda(dt);
+    }
+    aplicaRestricoesDireita(dt){
+    if (this.vx > 0) {
         const SIZE = this.cena.mapa.SIZE;
         if (this.vx > 0) {
           const pmx = this.mx + 1;
@@ -60,5 +65,26 @@ export default class Sprite{
           }
         }
       }
-   
+    }
+    aplicaRestricoesEsquerda(dt){
+        if (this.vx < 0) {
+          const SIZE = this.cena.mapa.SIZE;
+          const pmx = this.mx - 1;
+          const pmy = this.my;
+          if (this.cena.mapa.tiles[pmy][pmx] != 0) {
+            const tile = {
+              x: pmx * SIZE + SIZE / 2,
+              y: pmy * SIZE + SIZE / 2,
+              w: SIZE,
+              h: SIZE,
+            };
+            this.cena.ctx.strokeStyle = "white";
+            this.cena.ctx.strokeRect(tile.x-SIZE/2, tile.y-SIZE/2, SIZE, SIZE);
+            if (this.colidiuCom(tile)) {
+              this.vx = 0;
+              this.x = tile.x + tile.w / 2 + this.w / 2 + 1;
+            }
+          }
+        }
+      }
 }
