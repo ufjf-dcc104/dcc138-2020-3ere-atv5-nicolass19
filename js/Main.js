d3a76a694db7922fe3e66ffcfcf2264c30db5d94
Nicolas Moraes <nicolas.moraes@estudante.ufjf.br>
2021-03-18 17:33:01 -0300

    Adiciona mais teclas para controle do pc

--- js/Main.js
+++ js/Main.js
@@ -24,8 +24,10 @@ canvas.width = 14*32;
 canvas.height = 10*32;
 
 input.configurarTeclado({
-"ArrowLeft":"MOVE_ESQUERDA",
-"ArrowRight":"MOVE_DIREITA",
+ArrowLeft:"MOVE_ESQUERDA",
+ArrowRight:"MOVE_DIREITA",
+ArrowUp:"MOVE_CIMA",
+ArrowDown:"MOVE_BAIXO",
 }
 );
 
@@ -42,6 +44,13 @@ pc.controlar = function(dt){
         this.vx = +50;  
     }else{
         this.vx = 0;
+    }
+    if(input.comandos.get("MOVE_CIMA")){
+        this.vy = -50;
+    }else if(input.comandos.get("MOVE_BAIXO")){
+        this.vy = +50;  
+    }else{
+        this.vy = 0;
     }
 };
 const en1 = new Sprite({x:160, vx:-10, color:"red"});
